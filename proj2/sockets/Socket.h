7acfd4a18ae89285de63fcb487fd96a2de0fc704
PleaseSignIn <13982150015@139.com>
2020-05-21 19:52:24 +0800

    add code for the first time

---
+++ proj2/sockets/Socket.h
@@ -0,0 +1,19 @@
+#include<sys/socket.h>
+
+
+class Socket
+{
+private:
+    /* data */
+public:
+    Socket(/* args */);
+    ~Socket();
+};
+
+Socket::Socket(/* args */)
+{
+}
+
+Socket::~Socket()
+{
+}
